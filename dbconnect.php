1e40502623444243d5d698c9dcc358305023f873
Shrijal Kaphle <kaphle.shrijal9@gmail.com>
2020-03-09 08:44:20 +0545

    db connection error fixed

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,15 @@
 <?php
     session_start();
     $host = 'localhost';
+    
+    // username of database
     $user = 'root';
+    // $user = 'karmagurung2008';
+
+    // password of the database
     $pwd = '';
+    // $pwd = '123456';
+
     $db = 'sampleDB';
     // $db = $_SESSION['database'];
 
